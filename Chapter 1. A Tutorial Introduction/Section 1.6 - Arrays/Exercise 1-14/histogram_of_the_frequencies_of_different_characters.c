98d3704879363e4c94f94f95fef14f8612fe1ff1
Amir A. Shabani <amirshabani011@gmail.com>
2019-10-28 12:04:58 +0330

    get frequency instead of count

--- Chapter 1. A Tutorial Introduction/Section 1.6 - Arrays/Exercise 1-14/histogram_of_the_frequencies_of_different_characters.c
+++ Chapter 1. A Tutorial Introduction/Section 1.6 - Arrays/Exercise 1-14/histogram_of_the_frequencies_of_different_characters.c
@@ -7,6 +7,7 @@ main() {
 	int c;		/* read input character by character */
 	int freq[256];	/* frequencies of each ASCII character */
 	int i, j;	/* index to traverse through arrays */
+	int ccount = 0;	/* count the number of characters in input */
 
 	/* Initialize the array with 0 */
 	for(i = 0; i < 256; ++i)
@@ -14,8 +15,16 @@ main() {
 
 	/* fetch one character (until it reaches end of file
 	 * and increment its index in the array */
-	while((c = getchar()) != EOF)
+	while((c = getchar()) != EOF) {
 		++freq[c];
+		++ccount;
+	}
+
+	/* Divide each count by total count to get frequency */
+	for(i = 0; i < 256; ++i) {
+		freq[i] *= 100;
+		freq[i] /= ccount;
+	}
 
 	/* print those frequencies that are not zero */
 	for(i = 0; i < 256; ++i)
